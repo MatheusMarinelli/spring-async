d66a575f3ab0cf299cc4588571e01f1751489a6b
MatheusMarinelli <matheushlmarinelli@gmail.com>
2024-11-07 13:20:05 -0300

    adicionando novas configurações

--- src/main/java/br/com/java/thread/async/helper/request/AsyncRequest.java
+++ src/main/java/br/com/java/thread/async/helper/request/AsyncRequest.java
@@ -1,6 +1,7 @@
 package br.com.java.thread.async.helper.request;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -9,16 +10,18 @@ import org.springframework.web.client.RestTemplate;
 import java.util.concurrent.CompletableFuture;
 
 @Component
+@Slf4j
 @AllArgsConstructor
 public class AsyncRequest {
 
     @Autowired
     private RestTemplate restTemplate;
 
-    @Async
+    @Async // cria threads apartadas para executar o método abaixo baseado no método de configuração da classe AsyncConfig
     public CompletableFuture<Boolean> makeAsyncRequest(String url) {
         try {
             restTemplate.getForObject(url, String.class);
+            log.warn("Thread: " + Thread.currentThread().getName());
             return CompletableFuture.completedFuture(true);
         } catch (Exception e) {
             e.printStackTrace();
